75b3fa52dc4504e2795f51f2a09ccfed5c9a03ac
Андрей Александрович <Unknown7272527@gmail.com>
2020-03-19 02:54:07 +0300

    Добавил восклицательный знак

--- Проект в решении/Form1.cs
+++ Проект в решении/Form1.cs
@@ -17,7 +17,7 @@ namespace Проект_в_решении
         }
 
         private void button1_Click(object sender, EventArgs e) {
-            MessageBox.Show("Призываю GachiMuchi!");
+            MessageBox.Show("Призываю GachiMuchi!!");
         }
     }
 }
